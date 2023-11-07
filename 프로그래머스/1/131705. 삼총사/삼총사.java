eea15dc683636d67729159599a6e3e284b500307
털보 <hobakk12@gmail.com>
2023-11-08 01:36:25 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] number) {
+        int n = number.length;
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
