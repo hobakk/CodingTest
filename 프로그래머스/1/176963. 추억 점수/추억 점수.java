62524a8d4215f25661948bf3ea7c6c88fd4b5c8c
털보 <hobakk12@gmail.com>
2023-11-09 23:53:49 +0900

    [level 1] Title: 추억 점수, Time: 2.00 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -3,9 +3,11 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photos) {
         int[] result = new int[photos.length];
+        List<String> nameList = Arrays.asList(name);
+        
         for (int i=0; i < photos.length; i++) {
 			result[i] = Arrays.stream(photos[i])
-				.filter(p -> Arrays.asList(name).contains(p))
+				.filter(nameList::contains)
 				.map(p -> {
 					int index = 0;
 					for (int j=0; j < name.length; j++) {
