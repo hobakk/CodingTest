966e7c6314081e215ba77af19e2cd6687042be9c
털보 <hobakk12@gmail.com>
2023-12-09 14:03:46 +0900

    [level 2] Title: 기능개발, Time: 2.60 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,33 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
-        int[] works = new int[progresses.length];
-      
-        for(int i = 0; i < speeds.length; i++){
-            works[i]  = (100 - progresses[i]) / speeds[i];
-            if ((100 - progresses[i]) % speeds[i] != 0){
-                works[i]  += 1;
-            }
-        }
-        
-        int x = works[0];
-        int count=1;
-        for(int i=1;i<progresses.length;i++){
-            if(x>=works[i]) count+=1;
-            else {
-                list.add(count);
-                count =1;
-                x= works[i];
-            }
-        }
-        
-        list.add(count);
-        int[] answer = new int[list.size()];
-        for (int i = 0; i <list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+		Map<Integer, Integer> map = new HashMap<>();
+		int count = 0;
 
-        return answer;
-    }
+		for (int i=0; i < speeds.length; i++) {
+			while (progresses[i] < 100) {
+				count++;
+				progresses[i] += speeds[i];
+			}
+
+			map.put(i, count);
+			count = 0;
+		}
+
+		List<Integer> numbers = new ArrayList<>();
+		int date = 0;
+
+		for (int num : map.values()) {
+			if (date == 0) date = num;
+			else if (date >= num) count++;
+			else {
+				numbers.add(count + 1);
+				count = 0;
+                date = num;
+			}
+		}
+        
+        if (date > 0) numbers.add(count + 1);
+
+		return numbers.stream().mapToInt(Integer::intValue).toArray();
+	}
 }
