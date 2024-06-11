49fd072490054755cf8c5d46fca652f1ac5dd0ee
털보 <hobakk12@gmail.com>
2024-06-11 21:16:20 +0900

    [level 1] Title: 직사각형 별찍기, Time: 161.17 ms, Memory: 49.4 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
@@ -1,12 +1,8 @@
-import java.util.*
-
 fun main(args: Array<String>) {
-    val sc = Scanner(System.`in`)
     val sb = StringBuilder()
-    val str = sc.nextLine()
-    val list = str.split(" ")
-    (0 until list[1].toInt()).forEach { 
-        (0 until list[0].toInt()).forEach { i -> sb.append("*") }
+    val (a, b) = readLine()!!.split(" ").map(String::toInt)
+    (0 until b).forEach { 
+        (0 until a).forEach { i -> sb.append("*") }
         sb.append("\n")
     }
     
